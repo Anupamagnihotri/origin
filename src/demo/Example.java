5433cdc0331772109c0231d607a2444b95bcc42a
Anupam <anupamagnihotry2@gmail.com>
2016-01-05 11:34:57 +0530

    add files

---
+++ src/demo/Example.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String s="abc";
+		int i=1;
+		short s2=2;
+		float f=3.3f;
+		double d= 23.2;
+		char c='c';
+		
+		
+		
+		
+		System.out.print("hello "+s+" "+s2);
+		
+
+	}
+
+}
